fea1a7187f3c9f1e85156d090b91d35f90601da3
Nininanou16 <paul.peronredon@skypvp.fr>
2023-03-27 16:19:16 +0000

    add listen creation endpoint

---
+++ pages/api/listens/create.ts
@@ -0,0 +1,17 @@
+import connect from "next-connect";
+import checkJWT, { NextApiRequestWithUser } from "../../../src/middlewares/checkJWT";
+import prisma_instance from "../../../src/utils/prisma_instance";
+
+export default connect().post(checkJWT, async (req: NextApiRequestWithUser, res) => {
+    // if (!req.session.user.is_bot) {
+    //     res.status(403).send("forbidden")
+    //     return;
+    // }
+
+    let newListen: any = await prisma_instance.listens.create({
+        data: req.body
+    });
+
+    console.log(newListen);
+    res.status(200).send(newListen);
+});
