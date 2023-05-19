7356cee34780fe2aa3e8236f8f2696a91682b1e0
Paul PR <71654580+Nininanou16@users.noreply.github.com>
2023-05-19 12:41:51 +0200

    remove useless parameter

--- pages/listens/[id].tsx
+++ pages/listens/[id].tsx
@@ -16,7 +16,7 @@ import { SocketState } from "../../src/interfaces/socketState";
 
 type listenMessage = (messages & { accounts: { id: bigint; name: string; }; });
 
-export default function Listens(props){
+export default function Listens(){
   const router = useRouter();
   const listenSwr = useSWR<ListenWithStatusAndAccounts|null>(router.query.id?`/api/listens/${router.query.id}?not_done=true&with_users=true`:null, fetcherAuth);
   const [socketState, setSocketState] = useState<SocketState>(SocketState.deactivated);
