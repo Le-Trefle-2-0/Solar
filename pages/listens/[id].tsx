1b08a84831190fcaeae58b92a0cbde5e3408bd5d
Nininanou16 <paul.peronredon@skypvp.fr>
2023-05-31 20:04:45 +0000

    remove on listen assignment dropdown menu

--- pages/listens/[id].tsx
+++ pages/listens/[id].tsx
@@ -73,26 +73,6 @@ export default function Listens(){
     return classes.filter(Boolean).join(' ')
   }
 
-  console.log(session.current)
-
-  let people = [
-    {
-      id: 1,
-      name: 'Exemple 1'
-    }
-  ];
-  
-  if (available) {
-    let id = 1;
-    for (let i of available) {
-      id++;
-      people.push({
-        id: id,
-        name: i.name
-      });
-    }
-  }
-
   let [selected, setSelected] = useState(people[0]);
 
   return (
@@ -108,68 +88,6 @@ export default function Listens(){
           <div className="flex items-center justify-between mb-8">
             <div className="flex flex-row gap-4">
               <h2>ÉCOUTE {listen?.id}</h2>
-              <Listbox value={selected} onChange={setSelected}>
-                {({ open }) => (
-                  <>
-                    <div className="relative mt-2">
-                      <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                        <span className="flex items-center">
-                          <span className="ml-3 block truncate">{selected.name}</span>
-                        </span>
-                        <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                        </span>
-                      </Listbox.Button>
-
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        leave="transition ease-in duration-100"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                      >
-                        <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                          {people.map((person) => (
-                            <Listbox.Option
-                              key={person.id}
-                              className={({ active }) =>
-                                classNames(
-                                  active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                                  'relative cursor-default select-none py-2 pl-3 pr-9'
-                                )
-                              }
-                              value={person}
-                            >
-                              {({ selected, active }) => (
-                                <>
-                                  <div className="flex items-center">
-                                    <span
-                                      className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
-                                    >
-                                      {person.name}
-                                    </span>
-                                  </div>
-
-                                  {selected ? (
-                                    <span
-                                      className={classNames(
-                                        active ? 'text-white' : 'text-indigo-600',
-                                        'absolute inset-y-0 right-0 flex items-center pr-4'
-                                      )}
-                                    >
-                                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                    </span>
-                                  ) : null}
-                                </>
-                              )}
-                            </Listbox.Option>
-                          ))}
-                        </Listbox.Options>
-                      </Transition>
-                    </div>
-                  </>
-                )}
-              </Listbox>
             </div>
             <div className="flex">
               <button className="btn outlined" onClick={() => router.back()}>Retour a la liste</button>
