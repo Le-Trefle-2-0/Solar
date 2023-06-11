2410524c8310427cc3212fbe6e025109c119e7bb
Nininanou16 <paul.peronredon@skypvp.fr>
2023-06-11 23:38:37 +0200

    sidebar responsive #1

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 import { faCalendarAlt, faMessage } from "@fortawesome/free-regular-svg-icons";
-import { faAlignLeft, faAngleUp, faBook, faUsers, faChartColumn } from "@fortawesome/free-solid-svg-icons";
+import { faAlignLeft, faAngleUp, faBook, faUsers, faChartColumn, faBars } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 import logo from "../../../assets/img/logo.png"
 import Dropdown, {DropdownDirection} from "../dropdown";
@@ -39,49 +39,57 @@ export default function Nav(){
   }
 
   return(
-    <nav className="sidebar">
-      <div className="sidebar-logo"><Image src={logo}/></div>
-      <h4 className="sidebar-title">LE TREFLE 2.0</h4>
-      <div className="sidebar-links-wrapper">
-        {
-          (session.current?.user.is_admin || session.current?.user.is_ref) ?
-          <NavLink text="Écoutes" icon={faMessage} path="/listens"/> : null
-        }
-        {
-          session.current?.user.is_listener ? (listens.map((l,k) => (
-            <NavLink text={`Écoute ${l.id}`} icon={faMessage} path={`/listens/${l.id}`}/>
-          ))) : null
-        }
-        <NavLink text="Planning" icon={faCalendarAlt} path="/events"/>
-        {
-          showGlobalChatLink ? 
-            <NavLink text="Chat de permanence" icon={faAlignLeft} path="/globalChat" socket={eventCtx.globalChatSocket.current} socketEvent={ClientEvents.new_message}/>
-          : null
-        }
-        {
-          session.current?.user.is_admin ? 
-            <>
-              <NavLink text="Comptes" icon={faUsers} path="/accounts"/>
-              <NavLink text="Transcripts" icon={faBook} path="/transcripts"/>
-              <NavLink text="Statistiques" icon={faChartColumn} path="/stats"/>
-            </>
-          : null
-        }
+    <div className="responsive-sidebar">
+      <div className="sidebar-toggle" onClick={() => {
+        let sidebar = document.querySelector(".sidebar") as HTMLElement;
+        sidebar.classList.toggle("sidebar-open");
+      }}>
+        <FontAwesomeIcon icon={faBars}/>
       </div>
-      <div className="px-4 pb-6">
-        <Dropdown toggler={
-          <div className="font-medium px-4 py-2 rounded-2 hover:bg-trefle-green flex justify-between items-center">
-            {session.current?.user?.name ?? "Déconnecté"}
-            <FontAwesomeIcon icon={faAngleUp}/>
-          </div>
-        }
-        direction={DropdownDirection.top}>
-          <div className="btn white" onClick={()=>{
-            removeCookies("session");
-            router.reload();
-          }}>Déconnexion</div>
-        </Dropdown>
-      </div>
-    </nav>
+      <nav className="sidebar">
+        <div className="sidebar-logo"><Image src={logo}/></div>
+        <h4 className="sidebar-title">LE TREFLE 2.0</h4>
+        <div className="sidebar-links-wrapper">
+          {
+            (session.current?.user.is_admin || session.current?.user.is_ref) ?
+            <NavLink text="Écoutes" icon={faMessage} path="/listens"/> : null
+          }
+          {
+            session.current?.user.is_listener ? (listens.map((l,k) => (
+              <NavLink text={`Écoute ${l.id}`} icon={faMessage} path={`/listens/${l.id}`}/>
+            ))) : null
+          }
+          <NavLink text="Planning" icon={faCalendarAlt} path="/events"/>
+          {
+            showGlobalChatLink ? 
+              <NavLink text="Permanence" icon={faAlignLeft} path="/globalChat" socket={eventCtx.globalChatSocket.current} socketEvent={ClientEvents.new_message}/>
+            : null
+          }
+          {
+            session.current?.user.is_admin ? 
+              <>
+                <NavLink text="Comptes" icon={faUsers} path="/accounts"/>
+                <NavLink text="Transcripts" icon={faBook} path="/transcripts"/>
+                <NavLink text="Statistiques" icon={faChartColumn} path="/stats"/>
+              </>
+            : null
+          }
+        </div>
+        <div className="px-4 pb-6">
+          <Dropdown toggler={
+            <div className="font-medium px-4 py-2 rounded-2 hover:bg-trefle-green flex justify-between items-center">
+              {session.current?.user?.name ?? "Déconnecté"}
+              <FontAwesomeIcon icon={faAngleUp}/>
+            </div>
+          }
+          direction={DropdownDirection.top}>
+            <div className="btn white" onClick={()=>{
+              removeCookies("session");
+              router.reload();
+            }}>Déconnexion</div>
+          </Dropdown>
+        </div>
+      </nav>
+    </div>
   );
 }
