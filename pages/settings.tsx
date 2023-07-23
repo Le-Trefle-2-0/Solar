4a511333057df4d3bb4f29a29f6bb520c1b3fdb9
Nininanou16 <paul.peronredon@skypvp.fr>
2023-07-23 11:15:51 +0200

    create account settings edit component

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -7,9 +7,15 @@ import { useContext, useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 import { accounts, calendar_events, roles } from "@prisma/client";
 import fetcher from "../src/utils/fetcher";
-import AccountForm from "../src/components/form/accounts";
+import SettingsForm from "../src/components/form/settings";
+import Modal from "../src/components/modal";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Settings(props){
+interface ServersideProps{
+    rolesSSR: roles[]
+}
+
+export default function Settings({rolesSSR}: InferGetServerSidePropsType<typeof getServerSideProps>){
     const router = useRouter();
     const session = useRef(getSession());
     let [selectedAccountToEdit, setSelectedAccountToEdit] = useState<(Omit<accounts, "password"> & {roles: roles})|null>();
@@ -23,8 +29,19 @@ export default function Settings(props){
                 <button className="btn py-0.5 -my-1" onClick={() => {router.back()}}>Retour</button>
             </div>
             <div className="flex flex-col items-center mb-8 justify-between">
+                <h3>Informations du compte</h3>
                 
+                <button className="btn py-0.5 -my-1 mr-2" onClick={()=>setSelectedAccountToEdit(session.current?.user || null)}>Modifier</button>
             </div>
+            <Modal isOpened={selectedAccountToEdit !== undefined} title={`${selectedAccountToEdit != null ? "Modifier" : "Créer"} un compte`} onClose={()=>setSelectedAccountToEdit(undefined)}>
+                <div className="p-8">
+                    <SettingsForm key={Math.random()} account={selectedAccountToEdit ?? null} roles={rolesSSR} onCancel={()=>setSelectedAccountToEdit(undefined)} onSuccess={() => {setSelectedAccountToEdit(undefined); accountsSWR.mutate()}}/>
+                </div>
+            </Modal>
         </AuthenticatedLayout>
     );
+}
+
+export async function getServerSideProps(){
+    return {props: {rolesSSR: JSON.parse(JSON.stringify(await getRoles()))} as ServersideProps};
 }
