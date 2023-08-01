2b518cc433d109bb855e29d34c8871debac8e523
Nininanou16 <paul.peronredon@skypvp.fr>
2023-08-01 17:30:38 +0200

    handle 2fa authentication

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -2,18 +2,20 @@ import cryptoJS from "crypto-js";
 import Base64 from 'crypto-js/enc-base64';
 import connect from "next-connect";
 import { sign } from "jsonwebtoken";
+import totp from "totp-generator";
 import session, { sessionAccountWithRoles } from "../../../src/interfaces/session";
 import prisma_instance from "../../../src/utils/prisma_instance";
 import { object, string } from "yup";
 import checkSchema from "../../../src/middlewares/checkSchema";
 
-
 const schema = object({
   email: string().required(),
-  password: string().required()
-})
+  password: string().required(),
+  otp: string().optional()
+});
 
 export default connect().post(checkSchema({body: schema}), async (req, res) => {
+  console.log(req.body)
   let acc = await prisma_instance.accounts.findFirst({
     where: {
       password: Base64.stringify(cryptoJS.SHA512(req.body.password)),
@@ -25,14 +27,39 @@ export default connect().post(checkSchema({body: schema}), async (req, res) => {
   }) as sessionAccountWithRoles;
   if(acc){
     delete acc.password;
-    res.status(200).send({
-      jwt: sign(
-        acc,
-        process.env.JWT_SECRET || "secret",
-        {expiresIn: "1d"}
-      ),
-      user: acc
-    } as session);
+    console.log(acc)
+    if (req.body.otp && acc.otp_token) {
+      console.log(totp(acc.otp_token), req.body.otp)
+      if (totp(acc.otp_token) === req.body.otp) {
+        res.status(200).send({
+          jwt: sign(
+            acc,
+            process.env.JWT_SECRET || "secret",
+            {expiresIn: "1d"}
+          ),
+          otp: false,
+          user: acc
+        } as session);
+      } else {
+        res.status(401).send("invalid credentials");
+      }
+    } else if (acc.otp_enabled) {
+      res.status(200).send({
+        jwt: null,
+        otp: true,
+        user: acc
+      } as session);
+    } else {
+      res.status(200).send({
+        jwt: sign(
+          acc,
+          process.env.JWT_SECRET || "secret",
+          {expiresIn: "1d"}
+        ),
+        otp: false,
+        user: acc
+      } as session);
+    }
   }else{
     res.status(401).send("invalid credentials")
   }
--- src/interfaces/session.ts
+++ src/interfaces/session.ts
@@ -1,13 +1,19 @@
 import { accounts, roles } from "@prisma/client";
 
 export default interface session{
-    jwt: string , 
+    jwt: string | null,
+    otp: boolean,
     user: sessionAccountWithRoles
 }
 
-export type sessionAccountWithRoles = sessionAccount & {roles: roles, password?:string};
+export type sessionAccountWithRoles = sessionAccount & {
+    roles: roles, 
+    password?:string,
+    otp_secret?:string
+    otp_enabled?:boolean
+};
 
-export interface sessionAccount extends  Omit<accounts, 'password'> {
+export interface sessionAccount extends Omit<accounts, 'password'> {
     password?:string,
     is_admin: boolean,
     is_bot: boolean,
