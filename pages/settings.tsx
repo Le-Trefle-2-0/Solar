76756dd7873987c97c62f0c2a9612f4f1b41bfe2
Nininanou16 <paul.peronredon@skypvp.fr>
2023-08-14 00:13:26 +0200

    add 2fa settings modal

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 import AuthenticatedLayout from "../src/layouts/authenticated-layout";
+import TwoFactorAuthModal from "../src/components/form/2fa/enable";
+import random from "rand-token"
 import getSession from "../src/utils/get_session";
 import { getRoles } from "./api/roles";
 import { Line, Bar } from "react-chartjs-2";
@@ -18,9 +20,23 @@ interface ServersideProps{
 export default function Settings({rolesSSR}: InferGetServerSidePropsType<typeof getServerSideProps>){
     const router = useRouter();
     const session = useRef(getSession());
-    let [selectedAccountToEdit, setSelectedAccountToEdit] = useState<(Omit<accounts, "password"> & {roles: roles})|null>();
+    const [otpModalOpened, setOtpModalOpened] = useState<boolean>(false);
+    const b32 = random.generator({
+        chars: "base32"
+    });
+    const qrData = b32.generate(32);
+    let [selectedAccountToEdit, setSelectedAccountToEdit] = useState<(Omit<accounts, "password">)|null>();
     const accountsSWR = useSWR<(Omit<accounts, "password"> & {roles: roles})[]|null>("/api/accounts", fetcher);
     let accounts = accountsSWR.data || [];
+    
+    async function disable2FA() {
+        const disable2FA = await fetcher<string>('/api/2fa/disable', 'POST', {}).catch((e) => {
+            console.error(e);
+        });
+        if (disable2FA) {
+            router.push('/auth/login');
+        }
+    }
   
     return (
         <AuthenticatedLayout>
@@ -28,15 +44,37 @@ export default function Settings({rolesSSR}: InferGetServerSidePropsType<typeof 
                 <h2>Parametres</h2>
                 <button className="btn py-0.5 -my-1" onClick={() => {router.back()}}>Retour</button>
             </div>
-            <div className="flex flex-col items-center mb-8 justify-between">
-                <h3>Informations du compte</h3>
-                
-                <button className="btn py-0.5 -my-1 mr-2" onClick={()=>setSelectedAccountToEdit(session.current?.user || null)}>Modifier</button>
+            <div className="flex flex-row items-center mb-8 justify-between gap-8">
+                <div className="flex flex-col mb-8 justify-between gap-8">
+                    <h3>Informations du compte</h3>
+                    <input type="text" className="input" value={session.current?.user?.email} readOnly/>
+                    <input type="text" className="input" value={session.current?.user?.name} readOnly/>
+                    
+                    <button className="btn py-0.5 -my-1 mr-2" onClick={()=>setSelectedAccountToEdit(session.current?.user)}>Modifier</button>
+                </div>
+                <div className="flex flex-col items-left mb-8 justify-between gap-8">
+                    <h3>Authentification</h3>
+                    <button className="btn py-0.5 -my-1 mr-2" onClick={()=>router.push("/settings/password")}>Modifier le mot de passe</button>
+                    { session.current?.otp ?
+                        <div>
+                            <h4>Authentification á deux facteurs activée !</h4>
+                            <button className="btn py-0.5 -my-1 mr-2" onClick={()=>disable2FA()}>Désactiver l'authentification á deux facteurs</button>
+                        </div>
+                        :
+                        <div>
+                            <h4>Authentification á deux facteurs désactivée.<br/>Pour plus de sécurité, il est conseillé de l'activer.</h4>
+                            <button className="btn py-0.5 -my-1 mr-2" onClick={() => setOtpModalOpened(true)}>Activer l'authentification á deux facteurs</button>
+                        </div>
+                    }
+                </div>
             </div>
             <Modal isOpened={selectedAccountToEdit !== undefined} title={`${selectedAccountToEdit != null ? "Modifier" : "Créer"} un compte`} onClose={()=>setSelectedAccountToEdit(undefined)}>
                 <div className="p-8">
                     <SettingsForm key={Math.random()} account={selectedAccountToEdit ?? null} roles={rolesSSR} onCancel={()=>setSelectedAccountToEdit(undefined)} onSuccess={() => {setSelectedAccountToEdit(undefined); accountsSWR.mutate()}}/>
                 </div>
+            </Modal>
+            <Modal isOpened={otpModalOpened} title="Activer l'authentification á deux facteurs" onClose={()=>setOtpModalOpened(false)}>
+                <TwoFactorAuthModal isOpen={true} onClose={()=>{}} qrData={qrData} userID={session.current?.user.id}/>
             </Modal>
         </AuthenticatedLayout>
     );
---
+++ src/components/form/2fa/enable.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useRouter } from "next/router";
+import Modal from 'react-modal';
+import QRCode from 'qrcode.react';
+import fetcher from "../../../../src/utils/fetcher";
+
+interface TwoFactorAuthModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    qrData: string;
+    userID?: bigint;
+}
+
+export default function TwoFactorAuthModal({ isOpen, onClose, qrData, userID }: TwoFactorAuthModalProps) {
+  const router = useRouter();
+  const [error, setError] = React.useState<string | null>(null);
+  const [success, setSuccess] = React.useState<boolean>(false);
+  const [code, setCode] = React.useState<string>("");
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+      const res = await fetcher<string>('/api/2fa/enable', 'POST', {code, userID, token: qrData}).catch((e) => {
+          console.error(e);
+          setError(e.message);
+      });
+      if (res) {
+        if (res.startsWith('invalid')) return setError('Code invalide');
+        console.log(res);
+        setSuccess(true);
+        setTimeout(() => {
+          router.push('/auth/login')
+        }, 2000);
+      }
+  }
+
+  return (
+      <div className="modal-content flex flex-center flex-col p-1">
+        <QRCode className="w-screen" value={qrData} />
+        <p className="modal-text">
+          Code manuel :<br/>
+          <strong>{qrData}</strong>
+        </p>
+        <form className="modal-form flex flex-col flex-center" onSubmit={handleSubmit}>
+          <label htmlFor="2fa-code" className="modal-label">
+            Code A2F
+          </label>
+          <input id="2fa-code" onChange={
+              ({currentTarget:{value}})=>{setCode(value)}
+          } className="w-{80%} border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:border-trefle-green focus:ring-trefle-green focus:ring-opacity-50" type="text" />
+          <button className="btn mt-8">Activer</button>
+        </form>
+        {
+            error && <small className="text-red-500">{error}</small>
+        }
+        {
+            success && <small className="text-trefle-green">L'A2F a été activée avec succès !</small>
+        }
+      </div>
+  );
+};
