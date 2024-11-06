4a0b7e437c28a6a2b661e84aa58ad3b6f3d40b86
Nininanou16 <71654580+Nininanou16@users.noreply.github.com>
2024-11-06 23:29:16 +0100

    refactor socket creation

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,10 +1,10 @@
+import { Server, ServerOptions } from 'socket.io'
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Server,  ServerOptions } from 'socket.io'
 import http from "http";
-import SocketEvent from "../../src/socket/SocketEvent";
 import session from "../../src/interfaces/session";
-import socketAuth, { BotSession, ClientType, SessionType } from "../../src/socket/ServerActions/SocketAuth";
+import SocketEvent from "../../src/socket/SocketEvent";
 import io_data from "../../src/utils/io_data";
+import socketAuth, { BotSession, ClientType } from "../../src/socket/ServerActions/SocketAuth";
 
 type NextApiResponseWithSocket = NextApiResponse & {socket: {server: (Partial<ServerOptions> | http.Server | number) & { io: Server, ioData: IoData } }};
 export type IoData = {
@@ -21,26 +21,30 @@ export type IoData = {
     }[]
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponseWithSocket){
-    if (res.socket.server.io) {
-    } else {
-        console.log('[INFO] Socket is initializing')
-        const io = new Server(res.socket.server)
-        res.socket.server.io = io;
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
+  if (res.socket.server.io) {
+  } else {
+    console.log('[INFO] Socket is initializing')
+    const io = new Server(res.socket.server, {
+        addTrailingSlash: false
+    })
+    res.socket.server.io = io
 
-        io.on('connection', socket => {            
-            socket.onAny((eventName, ...args) => {
-                SocketEvent.dispatchEvent(socket, eventName, io_data, ...args);
-            });
+    io.on('connection', socket => {            
+        socket.onAny((eventName, ...args) => {
+            SocketEvent.dispatchEvent(socket, eventName, io_data, ...args);
+        });
 
-            socket.on('bot_connect', () => {
-                globalThis.botSocket = socket;
-            });
+        socket.on('bot_connect', () => {
+            globalThis.botSocket = socket;
+        });
 
-            socket.emit('hello', {});
+        socket.emit('hello', {});
 
-            socket.on("disconnect", () => socketAuth.removeSession(socket, io_data))
-        })
-    }
-    res.end()
+        socket.on("disconnect", () => socketAuth.removeSession(socket, io_data))
+    })
+  }
+  res.end()
 }
+
+export default SocketHandler
