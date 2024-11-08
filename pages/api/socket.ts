b2f032bc44f25d33fd68b8f986a8709770c4f7dd
Nininanou16 <71654580+Nininanou16@users.noreply.github.com>
2024-11-08 15:04:56 +0100

    edit socket initialization, major issue in io_data import to fix

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,12 +1,11 @@
 import { Server, ServerOptions } from 'socket.io'
-import type { NextApiRequest, NextApiResponse } from "next";
 import http from "http";
 import session from "../../src/interfaces/session";
+import type { NextApiRequest, NextApiResponse } from "next";
 import SocketEvent from "../../src/socket/SocketEvent";
 import io_data from "../../src/utils/io_data";
 import socketAuth, { BotSession, ClientType } from "../../src/socket/ServerActions/SocketAuth";
 
-type NextApiResponseWithSocket = NextApiResponse & {socket: {server: (Partial<ServerOptions> | http.Server | number) & { io: Server, ioData: IoData } }};
 export type IoData = {
     eventSessions: {
         socket_id: string,
@@ -21,27 +20,20 @@ export type IoData = {
     }[]
 }
 
+type NextApiResponseWithSocket = NextApiResponse & {socket: {server: (Partial<ServerOptions> | http.Server | number) & { io: Server, ioData: IoData } }};
+
 const SocketHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
   if (res.socket.server.io) {
+    console.log('Socket is already running')
   } else {
-    console.log('[INFO] Socket is initializing')
-    const io = new Server(res.socket.server, {
-        addTrailingSlash: false
-    })
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
     res.socket.server.io = io
 
     io.on('connection', socket => {            
         socket.onAny((eventName, ...args) => {
             SocketEvent.dispatchEvent(socket, eventName, io_data, ...args);
         });
-
-        socket.on('bot_connect', () => {
-            globalThis.botSocket = socket;
-        });
-
-        socket.emit('hello', {});
-
-        socket.on("disconnect", () => socketAuth.removeSession(socket, io_data))
     })
   }
   res.end()
