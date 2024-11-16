1740363476ad3cc7b8986cd4de75b5bec19fb535
Nininanou16 <71654580+Nininanou16@users.noreply.github.com>
2024-11-16 15:44:19 +0100

    move middleware in repo in accordance to newer nextjs version

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextResponse, NextRequest } from 'next/server';
-import checkJWT, { getSessionFromJWT } from '../src/middlewares/checkJWT';
+import checkJWT, { getSessionFromJWT } from './src/middlewares/checkJWT';
 
 export async function middleware(req: NextApiRequest & NextRequest, res: NextApiResponse & NextResponse) {
   const { href, pathname } = req.nextUrl
