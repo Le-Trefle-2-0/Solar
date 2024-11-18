f7464e403e2aadc0e8d164d0030df955abbda10b
Nininanou16 <paul@peronredon.eu>
2024-11-18 15:58:30 +0100

    add login schema validation

--- auth.ts
+++ auth.ts
@@ -3,6 +3,9 @@ import Credentials from "next-auth/providers/credentials"
 import * as crypto from "crypto";
 import prisma_instance from "@/utils/prisma_instance";
 import { TOTP } from "totp-generator";
+import { ZodError } from "zod";
+import { signInSchema } from "@/utils/zodValidation";
+import { error } from "console";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -13,26 +16,36 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             otptoken: {}
         },
         authorize: async (credentials) => {
-            let password = crypto.createHash('sha512').update(credentials?.password || "").digest('base64')
-            let user = await prisma_instance.accounts.findFirst({
-                where: {
-                    email: credentials?.email,
-                    password: password
-                }
-            });
+            try {
+                let user = null;
 
-            if (!user) {
-                throw new Error('Invalid credentials')
-            } else if (user && user.otp_enabled) {
-                const otp = TOTP.generate(user.otp_token || '');
-                if (otp.otp == credentials?.otptoken) {
-                    return user;
-                } else {
-                    throw new Error('Invalid 2FA code')
+                const { email, password } = await signInSchema.parseAsync(credentials)
+
+                let hashed = crypto.createHash('sha512').update(password).digest('base64')
+                user = await prisma_instance.accounts.findFirst({
+                    where: {
+                        email: email,
+                        password: hashed
+                    }
+                });
+    
+                if (!user) {
+                    throw new Error('Invalid credentials')
+                } else if (user && user.otp_enabled) {
+                    const otp = TOTP.generate(user.otp_token || '');
+                    if (otp.otp == credentials?.otptoken) {
+                        return user;
+                    } else {
+                        throw new Error('Invalid 2FA code')
+                    }
+                }
+    
+                return user;
+            } catch (e) {
+                if (error instanceof ZodError) {
+                    return null;
                 }
             }
-
-            return user;
         }
     })
   ],
