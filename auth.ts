ae5fa15a0248e7b7a8b2780c995d79d249b6f499
Nininanou16 <paul@peronredon.eu>
2025-01-09 18:34:27 +0100

    update auth.ts to handle api credentials checking

--- auth.ts
+++ auth.ts
@@ -1,52 +1,65 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
+import Discord from "next-auth/providers/discord"
 import * as crypto from "crypto";
-import prisma_instance from "@/utils/prisma_instance";
 import { TOTP } from "totp-generator";
 import { ZodError } from "zod";
-import { signInSchema } from "@/utils/zodValidation";
-import { error } from "console";
+import { signInSchema } from "@/src/utils/zodValidation";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Credentials({
-        credentials: {
-            email: {},
-            password: {},
-            otptoken: {}
-        },
-        authorize: async (credentials) => {
-            try {
-                let user = null;
+    // adapter: PrismaAdapter(prisma),
+    providers: [
+        // Discord
+        Credentials({
+            credentials: {
+                email: {},
+                password: {},
+                otptoken: {}
+            },
+            authorize: async (credentials) => {
+                try {
+                    let user = null;
 
-                const { email, password } = await signInSchema.parseAsync(credentials)
+                    const { email, password } = await signInSchema.parseAsync(credentials)
 
-                let hashed = crypto.createHash('sha512').update(password).digest('base64')
-                user = await prisma_instance.accounts.findFirst({
-                    where: {
-                        email: email,
-                        password: hashed
+                    let hashed = crypto.createHash('sha512').update(password).digest('base64')
+
+                    user = await fetch('http://localhost:3000/api/auth/login', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            email,
+                            password,
+                            otptoken: credentials?.otptoken
+                        })
+                    }).then(res => res.json())
+        
+                    if (!user) {
+                        throw new Error('Invalid credentials')
+                    } else if (user && user.otp_enabled) {
+                        const otp = TOTP.generate(user.otp_token || '');
+                        if (otp.otp == credentials?.otptoken) {
+                            return user;
+                        } else {
+                            throw new Error('Invalid 2FA code')
+                        }
                     }
-                });
-    
-                if (!user) {
-                    throw new Error('Invalid credentials')
-                } else if (user && user.otp_enabled) {
-                    const otp = TOTP.generate(user.otp_token || '');
-                    if (otp.otp == credentials?.otptoken) {
-                        return user;
-                    } else {
-                        throw new Error('Invalid 2FA code')
+                     
+        
+                    return user;
+                } catch (e) {
+                    if (e instanceof ZodError) {
+                        return null;
                     }
-                }
-    
-                return user;
-            } catch (e) {
-                if (error instanceof ZodError) {
-                    return null;
                 }
             }
-        }
-    })
-  ],
+        })
+    ],
+    pages: {
+        signIn: '/auth/login',
+    }
 })
