5a3ec411cb319d6d77f9eafa773fefea70046726
Nininanou16 <paul@peronredon.eu>
2025-01-09 18:33:45 +0100

    add discord login page

---
+++ pages/auth/discordLogin.tsx
@@ -0,0 +1,8 @@
+import LoginLayout from "../../src/layouts/login-layout";
+import SignIn from "@/src/components/discordLogin";
+
+export default function discordLogin(){
+    return(
+        SignIn()
+    )
+}
