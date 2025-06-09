261115ef42bcd848b759b0655fce8ab182e7247f
Nininanou16 <paul@peronredon.eu>
2025-06-09 12:27:13 +0200

    remove credit

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -153,7 +153,7 @@ export default function SignUp() {
                             await signUp.email({
                                 email,
                                 password,
-                                name: `${firstName} ${lastName}`,
+                                name: `${firstName} - ${lastName}`,
                                 image: image ? await convertImageToBase64(image) : "",
                                 callbackURL: "/dashboard",
                                 fetchOptions: {
@@ -181,13 +181,6 @@ export default function SignUp() {
                     </Button>
                 </div>
             </CardContent>
-            <CardFooter>
-                <div className="flex justify-center w-full border-t py-4">
-                    <p className="text-center text-xs text-neutral-500">
-                        Secured by <span className="text-orange-400">better-auth.</span>
-                    </p>
-                </div>
-            </CardFooter>
         </Card>
     );
 }
