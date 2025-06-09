ebb3cdd486c6119fae401bb6e3b61fc6f50b87ff
Nininanou16 <paul@peronredon.eu>
2025-06-09 12:27:43 +0200

    placeholder dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function Dashboard() {
-    return <div>Dashboard</div>;
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import SignOut from "@/components/auth/signout";
+import { authClient } from "@/lib/auth-client";
+
+export default async function Dashboard() {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        return <div>Not logged in</div>;
+    }
+    return <main className="flex flex-col gap-3 items-center justify-center p-10">
+        <div className="flex gap-3">
+            <SignOut />
+        </div>
+        <p>{session.user.name}</p>
+    </main>;
 }
