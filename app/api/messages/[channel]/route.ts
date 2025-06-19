c4969260088b16b1ddec6731b05acdc17e3d7fdd
Nininanou16 <paul@peronredon.eu>
2025-06-19 14:31:23 +0200

    re-enable session validation for messages fetching

--- app/api/messages/[channel]/route.ts
+++ app/api/messages/[channel]/route.ts
@@ -1,4 +1,3 @@
-import prisma from "@/lib/prisma";
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
 import {Msg} from "@/lib/interface";
@@ -11,17 +10,12 @@ export async function GET(
     const session = await auth.api.getSession({
         headers: await headers()
     });
-    // if (!session) {
-    //     return new Response('Unauthorized', {
-    //         status: 401,
-    //     });
-    // }
+    if (!session) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    }
     const {channel} = await params;
-    const message = await prisma.message.findMany({
-        where: {
-            channelId: channel
-        }
-    });
 
     let messages: Msg[] = await getMessages(channel);
     return Response.json(messages);
