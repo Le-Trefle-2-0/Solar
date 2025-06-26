3cd1b3fd9611dc9d08e0508d79f6d087fe68add7
Nininanou16 <paul@peronredon.eu>
2025-06-26 15:13:44 +0200

    update allowed origins

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
   /* config options here */
     images: {
         remotePatterns: [new URL("https://media.tenor.com/**")]
-    }
+    },
+    devIndicators: false,
+    allowedDevOrigins: ["20.199.66.229:3000", "20.199.66.229"]
 };
 
 export default withFlowbiteReact(nextConfig);
