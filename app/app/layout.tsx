ebf62c70c4b3e0af0d11e2cabdc3d6719c8f3009
Nininanou16 <paul@peronredon.eu>
2025-06-29 12:04:07 +0200

    configure app redirects

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,17 +1,24 @@
 import type {Metadata} from "next";
 import {SidebarProvider, SidebarTrigger} from "@/components/ui";
 import {AppSidebar} from "@/components/app-sidebar";
+import {redirect} from "next/navigation";
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
 
 export const metadata: Metadata = {
     title: "Solar - Le Trèfle 2.0",
     description: "Logiciel d'écoute",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
                                        children,
                                    }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+    if (!session) redirect('/auth/sign-in')
     return (
         <SidebarProvider>
             <AppSidebar className="border-r-main border-r"/>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,11 @@
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
-import SignOut from "@/components/auth/signout";
+import {redirect} from "next/navigation";
 
 export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  return (
-      <div>
-          <main className="flex flex-col gap-3 items-center justify-center p-10">
-              <div className="flex gap-3">
-                  <SignOut />
-              </div>
-              <p>{!session ? "Not authenticated" : session.user.name}</p>
-          </main>
-      </div>
-  );
+  return redirect('/app')
 }
