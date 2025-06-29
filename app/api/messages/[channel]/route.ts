66b8304a339841388140bb6344b3160511e695a6
Nininanou16 <paul@peronredon.eu>
2025-06-29 12:04:28 +0200

    add message publishing endpoint

--- app/api/messages/[channel]/route.ts
+++ app/api/messages/[channel]/route.ts
@@ -1,6 +1,9 @@
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
-import {getMessages} from "@/lib/messageManager";
+import {getMessages, saveMessage} from "@/lib/messageManager";
+import {NextRequest, NextResponse} from "next/server";
+import {z} from "zod";
+import {Msg} from "@/lib/interface";
 
 export async function GET(
     request: Request,
@@ -40,4 +43,40 @@ export async function GET(
 
     const messages = await getMessages(channel);
     return Response.json(messages);
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    const messageSchema = z.object({
+        author: z.object({
+            id: z.string(),
+            name: z.string(),
+            image: z.string(),
+        }),
+        content: z.string(),
+        timestamp: z.number(),
+        channel: z.object({
+            id: z.string(),
+        }),
+        token: z.string(),
+    })
+    try {
+        const verifiedBody = messageSchema.parse(body);
+        const {valid, error, key} = await auth.api.verifyApiKey({
+            body: {
+                key: verifiedBody.token
+            }
+        });
+
+        if (!valid) return new Response('Unauthorized', {status: 401});
+
+        const message = await saveMessage(verifiedBody as Msg);
+        return NextResponse.json({success: true, message}, {status: 200});
+
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            return NextResponse.json({success: false, errors: err.flatten()}, {status: 400});
+        }
+        return NextResponse.json({success: false, message: "Internal server error"}, {status: 500});
+    }
 }
