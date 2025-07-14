29727f2a5cb875d97518a7acbd0f69ccd944df37
Nininanou16 <paul@peronredon.eu>
2025-07-14 14:56:07 +0200

    remove unused import

--- components/chat.tsx
+++ components/chat.tsx
@@ -2,7 +2,6 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faCircle} from "@fortawesome/free-solid-svg-icons";
 import GifPicker from "gif-picker-react";
-// import EmojiPicker, {EmojiStyle} from "emoji-picker-react";
 import {EmojiPicker, EmojiPickerContent, EmojiPickerFooter, EmojiPickerSearch,} from "@/components/ui/emoji-picker";
 import {
     Check,
