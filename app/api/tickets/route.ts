2511d296a15c4db1b6aa7b138211fc2f189a819e
Nininanou16 <paul@peronredon.eu>
2025-07-26 00:31:23 +0200

    add permissions and assignment support on ticket list fetching endpoint

--- app/api/tickets/route.ts
+++ app/api/tickets/route.ts
@@ -2,11 +2,14 @@ import {NextRequest, NextResponse} from "next/server";
 import prisma from "@/lib/prisma";
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
+import {Ticket} from "@/generated/prisma";
 
 export async function GET(req: NextRequest) {
     const session = await auth.api.getSession({
         headers: await headers()
     });
+
+    let tickets: Ticket[] = [];
     if (!session) {
         const reqHeaders = await headers()
         const {valid, error, key} = await auth.api.verifyApiKey({
@@ -19,7 +22,36 @@ export async function GET(req: NextRequest) {
             status: 401,
         });
     }
-    let tickets = await prisma.ticket.findMany();
 
-    return NextResponse.json(tickets, {status: 200});
+    const perm = await auth.api.userHasPermission({
+        body: {
+            userId: session?.user.id,
+            permission: {
+                tickets: ["read_all"]
+            }
+        }
+    });
+
+    if (!perm.success) {
+        console.log("NO READ ALL PERM");
+        tickets = await prisma.ticket.findMany({
+            where: {
+                assignedUserId: session?.user.id,
+                statusName: {
+                    in: ["started", "closed"]
+                }
+            }
+        });
+
+        return NextResponse.json({success: true, tickets}, {status: 200});
+    }
+
+    tickets = await prisma.ticket.findMany({
+        where: {
+            statusName: {
+                in: ["waiting", "started", "closed"]
+            }
+        }
+    });
+    return NextResponse.json({success: true, tickets}, {status: 200});
 }
