ed598a01dad5ff164d8c182c262447dc8c0ad927
Nininanou16 <paul@peronredon.eu>
2025-07-26 01:50:59 +0200

    update prima migrations

---
+++ prisma/migrations/20250720195913_add_image_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Image`
+(
+    `id`   VARCHAR(191) NOT NULL,
+    `link` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ prisma/migrations/20250720200510_change_image_link_to_longtext/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Image` MODIFY `link` LONGTEXT NOT NULL;
