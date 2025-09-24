a5f42c585ef14dd921c1adfcaa1c967849a6c7bd
Nininanou16 <paul@peronredon.eu>
2025-09-24 16:03:45 +0200

    add message management permission

--- lib/permissions.ts
+++ lib/permissions.ts
@@ -6,6 +6,7 @@ export const statement = {
     tickets: ['open', 'close', 'read_all', 'attribute', 'launch_voice', 'send_message_all', 'send_message', 'transmission'],
     management: ['create_account', 'delete_account', 'ticket_history', 'view_transmission', 'view_stats', 'reset_password'],
     permanence: ['open', 'close', 'register', 'unregister', 'unregister_other_all', 'unregister_other_user', 'register_other_all', 'register_other_user'],
+    messages: ['manage']
 } as const;
 
 export const ac = createAccessControl(statement);
@@ -25,7 +26,8 @@ export const volunteer = ac.newRole({
 export const manager = ac.newRole({
     tickets: ['close', 'read_all', 'attribute', 'launch_voice', 'send_message_all'],
     management: ['ticket_history', 'view_transmission', 'view_stats'],
-    permanence: ['open', 'close', 'register', 'unregister', 'register_other_user', 'unregister_other_user']
+    permanence: ['open', 'close', 'register', 'unregister', 'register_other_user', 'unregister_other_user'],
+    messages: ['manage']
 });
 
 export const bot = ac.newRole({
@@ -37,4 +39,5 @@ export const admin = ac.newRole({
     tickets: ['open', 'close', 'read_all', 'attribute', 'launch_voice', 'send_message_all', 'send_message', 'transmission'],
     management: ['create_account', 'delete_account', 'ticket_history', 'view_transmission', 'view_stats', 'reset_password'],
     permanence: ['open', 'close', 'register', 'unregister', 'unregister_other_all', 'unregister_other_user', 'register_other_all', 'register_other_user'],
+    messages: ['manage']
 });
