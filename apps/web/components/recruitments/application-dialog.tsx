7f50179986b23533bd3d888400300526c1832c00
PaulPR-44 <paul@peronredon.eu>
2025-12-24 12:13:00 +0100

    typerror fix

--- apps/web/components/recruitments/application-dialog.tsx
+++ apps/web/components/recruitments/application-dialog.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import {Button} from "@/components/ui/button";
 import {ApplicationForm} from "./application-form";
-import {RecruitmentField} from "./field-manager";
+import type {RecruitmentField} from "@/lib/recruitments";
 import {Send} from "lucide-react";
 
 interface ApplicationDialogProps {
